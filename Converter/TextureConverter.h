c7d448921b0c30cd7c7f7442b0f9cffcd8b19529
pepe <kousaku5963@gmail.com>
2024-10-28 15:10:39 +0900

    テクスチャファイルをDDSにして出力できるようにした

---
+++ Converter/TextureConverter.h
@@ -0,0 +1,51 @@
+#pragma once
+#include <string>
+#include <Windows.h>
+#include <DirectXTex.h>
+
+class TextureConverter {
+public:
+	/// <summary>
+	/// テクスチャをWICからDDSに変換する
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	void ConverterTextureWICToDDS(const std::string& filePath);
+
+private:// プライベートなメンバ関数
+	/// <summary>
+	/// テクスチャファイル読み込み
+	/// </summary>
+	/// <param name="filePath"></param>
+	void LoadWICTextureFromFile(const std::string& filePath);
+
+	/// <summary>
+	/// マルチバイト文字列をワイド文字列に変換
+	/// </summary>
+	/// <param name="mString">マルチバイト文字列</param>
+	/// <returns></returns>
+	static std::wstring ConvertMultiByteStringToWideString(const std::string& mString);
+
+	/// <summary>
+	/// フォルダパスとファイル名を分離
+	/// </summary>
+	/// <param name="filePath">ファイルパス</param>
+	void SeparateFilePath(const std::wstring& filePath);
+
+	/// <summary>
+	/// DDSテクスチャとしてファイル書き出し
+	/// </summary>
+	void SaveDDSTextureToFile();
+
+private:
+	// 画像の情報
+	DirectX::TexMetadata metadata_;
+	// 画像イメージのコンテナ
+	DirectX::ScratchImage scratchImage_;
+
+	// ディレクトリパス
+	std::wstring directoryPath_;
+	// ファイル名
+	std::wstring fileName_;
+	// ファイル拡張子
+	std::wstring fileExt_;
+};
--- main.cpp
+++ main.cpp
@@ -1,8 +1,32 @@
+#include "Converter/TextureConverter.h"
+
+#include <assert.h>
 #include <cstdio>
 #include <cstdlib>
+#include <Windows.h>
 
-int main() {
-	printf("Hellow,World!\n");
+enum Argument {
+	kApplicationPath,	// アプリケーションのパス
+	kFilePath,			// 渡されたファイルのパス
+
+	NumArgument
+};
+
+int main(int argc, char* argv[]) {	
+	assert(argc >= NumArgument);
+
+	// COMライブラリの初期化
+	HRESULT hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+	assert(SUCCEEDED(hr));
+
+	// テクスチャコンバータ
+	TextureConverter converter;
+
+	// テクスチャ変換
+	converter.ConverterTextureWICToDDS(argv[kFilePath]);
+
+	// COMライブラリの終了
+	CoUninitialize();
 
 	system("pause");
 	return 0;
